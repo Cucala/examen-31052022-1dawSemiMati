8a8a90f5bd6ef67c03fa89cb7501a384a0bcc12c
James <98335470+byjamesmars@users.noreply.github.com>
2022-05-31 11:50:02 +0200

    form2 Terminador

---
+++ JaimeMartinezMartinez34/Form2.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace JaimeMartinezMartinez34
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNombreProd = new System.Windows.Forms.Label();
+            this.lblDescrProd = new System.Windows.Forms.Label();
+            this.lblCategoriaProd = new System.Windows.Forms.Label();
+            this.lblPrecioProd = new System.Windows.Forms.Label();
+            this.txtBxNombreProd = new System.Windows.Forms.TextBox();
+            this.txtBoxDescrProd = new System.Windows.Forms.TextBox();
+            this.txtBoxCatProducto = new System.Windows.Forms.TextBox();
+            this.txtBoxPrecioProd = new System.Windows.Forms.TextBox();
+            this.bttnGuardar = new System.Windows.Forms.Button();
+            this.bttnCancelar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // lblNombreProd
+            // 
+            this.lblNombreProd.AutoSize = true;
+            this.lblNombreProd.Location = new System.Drawing.Point(70, 97);
+            this.lblNombreProd.Name = "lblNombreProd";
+            this.lblNombreProd.Size = new System.Drawing.Size(67, 20);
+            this.lblNombreProd.TabIndex = 0;
+            this.lblNombreProd.Text = "Nombre:";
+            // 
+            // lblDescrProd
+            // 
+            this.lblDescrProd.AutoSize = true;
+            this.lblDescrProd.Location = new System.Drawing.Point(71, 149);
+            this.lblDescrProd.Name = "lblDescrProd";
+            this.lblDescrProd.Size = new System.Drawing.Size(90, 20);
+            this.lblDescrProd.TabIndex = 1;
+            this.lblDescrProd.Text = "Descripcion:";
+            // 
+            // lblCategoriaProd
+            // 
+            this.lblCategoriaProd.AutoSize = true;
+            this.lblCategoriaProd.Location = new System.Drawing.Point(72, 202);
+            this.lblCategoriaProd.Name = "lblCategoriaProd";
+            this.lblCategoriaProd.Size = new System.Drawing.Size(74, 20);
+            this.lblCategoriaProd.TabIndex = 2;
+            this.lblCategoriaProd.Text = "Categoria";
+            // 
+            // lblPrecioProd
+            // 
+            this.lblPrecioProd.AutoSize = true;
+            this.lblPrecioProd.Location = new System.Drawing.Point(74, 254);
+            this.lblPrecioProd.Name = "lblPrecioProd";
+            this.lblPrecioProd.Size = new System.Drawing.Size(53, 20);
+            this.lblPrecioProd.TabIndex = 3;
+            this.lblPrecioProd.Text = "Precio:";
+            // 
+            // txtBxNombreProd
+            // 
+            this.txtBxNombreProd.Location = new System.Drawing.Point(239, 95);
+            this.txtBxNombreProd.Name = "txtBxNombreProd";
+            this.txtBxNombreProd.Size = new System.Drawing.Size(125, 27);
+            this.txtBxNombreProd.TabIndex = 4;
+            // 
+            // txtBoxDescrProd
+            // 
+            this.txtBoxDescrProd.Location = new System.Drawing.Point(236, 150);
+            this.txtBoxDescrProd.Name = "txtBoxDescrProd";
+            this.txtBoxDescrProd.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxDescrProd.TabIndex = 5;
+            // 
+            // txtBoxCatProducto
+            // 
+            this.txtBoxCatProducto.Location = new System.Drawing.Point(237, 202);
+            this.txtBoxCatProducto.Name = "txtBoxCatProducto";
+            this.txtBoxCatProducto.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxCatProducto.TabIndex = 6;
+            // 
+            // txtBoxPrecioProd
+            // 
+            this.txtBoxPrecioProd.Location = new System.Drawing.Point(235, 251);
+            this.txtBoxPrecioProd.Name = "txtBoxPrecioProd";
+            this.txtBoxPrecioProd.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxPrecioProd.TabIndex = 7;
+            // 
+            // bttnGuardar
+            // 
+            this.bttnGuardar.Location = new System.Drawing.Point(117, 332);
+            this.bttnGuardar.Name = "bttnGuardar";
+            this.bttnGuardar.Size = new System.Drawing.Size(94, 29);
+            this.bttnGuardar.TabIndex = 8;
+            this.bttnGuardar.Text = "Guardar";
+            this.bttnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // bttnCancelar
+            // 
+            this.bttnCancelar.Location = new System.Drawing.Point(306, 332);
+            this.bttnCancelar.Name = "bttnCancelar";
+            this.bttnCancelar.Size = new System.Drawing.Size(94, 29);
+            this.bttnCancelar.TabIndex = 9;
+            this.bttnCancelar.Text = "Cancelar";
+            this.bttnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Añade Producto";
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(479, 421);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bttnCancelar);
+            this.Controls.Add(this.bttnGuardar);
+            this.Controls.Add(this.txtBoxPrecioProd);
+            this.Controls.Add(this.txtBoxCatProducto);
+            this.Controls.Add(this.txtBoxDescrProd);
+            this.Controls.Add(this.txtBxNombreProd);
+            this.Controls.Add(this.lblPrecioProd);
+            this.Controls.Add(this.lblCategoriaProd);
+            this.Controls.Add(this.lblDescrProd);
+            this.Controls.Add(this.lblNombreProd);
+            this.Name = "Form2";
+            this.Text = "Hola soy el TPV rey";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label lblNombreProd;
+        private Label lblDescrProd;
+        private Label lblCategoriaProd;
+        private Label lblPrecioProd;
+        private TextBox txtBxNombreProd;
+        private TextBox txtBoxDescrProd;
+        private TextBox txtBoxCatProducto;
+        private TextBox txtBoxPrecioProd;
+        private Button bttnGuardar;
+        private Button bttnCancelar;
+        private Label label3;
+    }
+}
